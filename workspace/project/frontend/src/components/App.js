d6ebf1b01b685bab371cf74b0cbb4dacdfbdeefc
tocchy-tocchy <tochigi-reina@g.ecc.u-tokyo.ac.jp>
2020-10-17 16:16:00 +0900

    トップページほぼ完成（リンク以外）

--- workspace/project/frontend/src/components/App.js
+++ workspace/project/frontend/src/components/App.js
@@ -6,15 +6,18 @@ const App = () => {
       <div class="catch brownfont">
         <h1>今日のおつまみ、<br />何にしよう。</h1>
         <br />
-        <br />
         <p class="title_sentence brownfont">
           今日飲む予定のお酒に合わせて、<br />
-        AIがおつまみを提案。<br />
+          AIがおつまみを提案。<br />
           <br />
-        写真を撮って質問に答えるだけで、<br />
-        あなたの今日のおつまみが決まります！
-      </p>
-        <a href="#howto">howto</a>
+          写真を撮って質問に答えるだけで、<br />
+          あなたの今日のおつまみが決まります！<br />
+          <ul class="top_menu">
+            <li><a href="#howto" class="btn-large waves-effect waves-light brown darken-3">使い方</a></li>
+            <li><a href="#howto" class="btn-large waves-effect waves-light brown darken-3">おつまみを決める</a></li>
+          </ul>
+        </p>
+
       </div>
     </div>
   );
