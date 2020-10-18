a74bd8ab7342c342fc2fd083db9df98306d59633
tocchy-tocchy <tochigi-reina@g.ecc.u-tokyo.ac.jp>
2020-10-18 11:57:18 +0900

    CSSフレームワーク変更。index完成。submit、quizは概略のみ。

---
+++ workspace/project/frontend/src/components/Slide.js
@@ -0,0 +1,99 @@
+// /* import React from "react";
+// import yakitori from '../../static/frontend/img/slide/yakitori.jpg';
+// import beef from '../../static/frontend/img/slide/beef.jpg';
+// import fish from '../../static/frontend/img/slide/fish.jpg';
+
+// class Slide extends React.Component {
+//   render() {
+//     return (
+//       /*      <div className="slider">
+//              <ul className="slides">
+//                <li>
+//                  <img src={yakitori} />
+//                  <div class="caption center-align">
+//                    <h3>Slider</h3>
+//                    <h5 class="light grey-text text-lighten-3">1枚目</h5>
+//                  </div>
+//                </li>
+//                <li>
+//                  <img src={beef} />
+//                  <div class="caption left-align">
+//                    <h3>Slider</h3>
+//                    <h5 class="light grey-text text-lighten-3">2枚目</h5>
+//                  </div>
+//                </li>
+//                <li>
+//                  <img src={fish} />
+//                  <div class="caption right-align">
+//                    <h3>Slider</h3>
+//                    <h5 class="light grey-text text-lighten-3">3枚目</h5>
+//                  </div>
+//                </li>
+//              </ul>
+//            </div> */
+
+
+// <div class='csslider1 autoplay '>
+//   <input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide' />
+//   <input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide' />
+//   <input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide' />
+//   <input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked />
+//   <input name="cs_anchor1" id='cs_pause1' type="radio" class='cs_anchor' />
+//   <ul>
+//     <div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;">
+//       <img src={yakitori} style="width: 100%;" />
+//     </div>
+//     <li class='num0 img'>
+//       <img src={yakitori} alt='Buns' title='Buns' />
+//     </li>
+//     <li class='num1 img'>
+//       <img src={beef} alt='Croissant' title='Croissant' />
+//     </li>
+//     <li class='num2 img'>
+//       <img src={fish} alt='Lemon pie' title='Lemon pie' />
+//     </li>
+
+//   </ul>
+//   <div class='cs_description'>
+//     <label class='num0'>
+//       <span class="cs_title"><span class="cs_wrapper">Buns</span></span>
+//     </label>
+//     <label class='num1'>
+//       <span class="cs_title"><span class="cs_wrapper">Croissant</span></span>
+//     </label>
+//     <label class='num2'>
+//       <span class="cs_title"><span class="cs_wrapper">Lemon pie</span></span>
+//     </label>
+//   </div>
+
+//   <div class='cs_arrowprev'>
+//     <label class='num0' for='cs_slide1_0'></label>
+//     <label class='num1' for='cs_slide1_1'></label>
+//     <label class='num2' for='cs_slide1_2'></label>
+//   </div>
+//   <div class='cs_arrownext'>
+//     <label class='num0' for='cs_slide1_0'></label>
+//     <label class='num1' for='cs_slide1_1'></label>
+//     <label class='num2' for='cs_slide1_2'></label>
+//   </div>
+
+//   <div class='cs_bullets'>
+//     <label class='num0' for='cs_slide1_0'>
+//       <span class='cs_point'></span>
+//       <span class='cs_thumb'><img src={yakitori} alt='Buns' title='Buns' /></span>
+//     </label>
+//     <label class='num1' for='cs_slide1_1'>
+//       <span class='cs_point'></span>
+//       <span class='cs_thumb'><img src={beef} alt='Croissant' title='Croissant' /></span>
+//     </label>
+//     <label class='num2' for='cs_slide1_2'>
+//       <span class='cs_point'></span>
+//       <span class='cs_thumb'><img src={fish} alt='Lemon pie' title='Lemon pie' /></span>
+//     </label>
+//   </div>
+// </div>
+//     );
+//   }
+// }
+// export default Slide;
+//  * /
--- workspace/project/frontend/src/index.js
+++ workspace/project/frontend/src/index.js
@@ -1,8 +1,12 @@
 import App from "./components/App";
-//import Head from "./components/Head";
+import Top from "./components/Top";
+import Pic from "./components/Pic";
+import Question from "./components/Question"
 import React from "react";
 import ReactDOM from "react-dom";
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
-//ReactDom.render(<Head />, document.getElementById("head"));
+ReactDOM.render(<Top />, document.getElementById("top"));
+ReactDOM.render(<Pic />, document.getElementById("pic"));
+ReactDOM.render(<Question />, document.getElementById("question"));
